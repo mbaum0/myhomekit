43e852bdac65fa27c1d944d2014f4362071d7752
Michael Baumgarten <mbaum0@pm.me>
2021-11-04 16:53:30 -0600

    init commit

---
+++ MyHomeKit/MyHomeKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyHomeKitApp.swift
+//  MyHomeKit
+//
+//  Created by Michael Baumgarten on 11/4/21.
+//
+
+import SwiftUI
+
+@main
+struct MyHomeKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
